7bc495002bfe1a00641072bc4cabc193bc7d55a0
TANG Kai Yui <yuitang2-c@ad.cityu.edu.hk>
2018-11-12 11:32:52 +0800

    LinkedList ver.

--- Starlight/main.cpp
+++ Starlight/main.cpp
@@ -4,143 +4,132 @@
 
 using namespace std;
 
-class DynamicArray {
-private:
-	int size;
-	int capacity;
-	string *storage;
+class node {
 public:
-	DynamicArray() {
-		capacity = 1100;
-		size = 0;
-		storage = new string[capacity];
-	}
-	DynamicArray(int capacity) {
-		this->capacity = capacity;
-		size = 0;
-		storage = new string[capacity];
-	}
-	~DynamicArray() {
-		delete[] storage;
-	}
-	void setCapacity(int newCapacity) {
-		//https://stackoverflow.com/questions/4979681/how-to-properly-free-a-stdstring-from-memory
-		string *newStorage = new string[newCapacity];
-		memcpy(newStorage, storage, sizeof(string)*size);
-		capacity = newCapacity;
-		storage = newStorage;
-	}
-	//edited...?
-	void ensureCapacity(int minCapacity)
-	{
-		if (minCapacity > capacity) {
-			//50% buffer
-			int newCapacity = (capacity * 3) / 2 + 1;
-			if (newCapacity < minCapacity)
-				newCapacity = minCapacity;
-			setCapacity(newCapacity);
+	string tconst;
+	string titleType;
+	string primaryTitle;
+	string startYear;
+	string runtimeMinutes;
+	//I need a counter for genres
+	int genresCounter;
+	string* genres;
+	node* next;
+public:
+	node(string tconst, string titleType, string primaryTitle, string startYear, string runtimeMinutes, string genres) {
+		this->tconst = tconst;
+		this->titleType = titleType;
+		this->primaryTitle = primaryTitle;
+		this->startYear = startYear;
+		this->runtimeMinutes = runtimeMinutes;
+		this->next = NULL;
+		//process genres
+		int genresNo = 1;
+		for (int i = 0; i < genres.size(); i++) {
+			if (genres[i] == 44)	genresNo++;
 		}
+		this->genres = new string[genresNo];
+		string soloGenres;
+		//debug message
+		//cout << genres << endl;
+		int readGenresInOrder = 0;
+		for (int i = 0; i < genres.size(); i++) {
+			if (genres[i] == 44 || (genres[i] == 34 && i != 0)) {
+				this->genres[readGenresInOrder].assign(soloGenres);
+				readGenresInOrder = readGenresInOrder + 1;
+				//debug message
+				//cout << soloGenres << endl;
+				soloGenres.erase();
+			}
+			else if (genres[i + 1] == NULL) {
+				soloGenres += genres[i];
+				this->genres[readGenresInOrder].assign(soloGenres);
+				readGenresInOrder = readGenresInOrder + 1;
+				//debug message
+				//cout << soloGenres << endl;
+				soloGenres.erase();
+			}
+			else {
+				if (genres[i] != 34)
+					soloGenres += genres[i];
+			}
+		}
+		this->genresCounter = genresNo;
 	}
-	//bug exist after deleted a record
-	void rangeCheck(int index) {
-		if (index < 0 || index >= size)
-			throw "Index out of bounds!";
-	}
-	void set(int index, int value) {
-		rangeCheck(index);
-		storage[index] = value;
-	}
-	string get(int index) {
-		rangeCheck(index);
-		return storage[index];
-	}
-	void removeAt(int index) {
-		rangeCheck(index);
-		int moveCount = size - index - 1;
-		if (moveCount > 0)
-			memmove(storage + index, storage + (index + 1), sizeof(string) * moveCount);
-		size--;
-		//pack();
-	}
-	void insertAt(int index, string content) {
-		if (index < 0 || index > size)
-			throw "Index out of bounds!";
-		ensureCapacity(size + 1);
-		int moveCount = size - index;
-		if (moveCount != 0)
-			memmove(storage + (index + 1), storage + index, sizeof(string) * moveCount);
-		storage[index] = content;
-		size++;
-	}
+};
 
-	void searchByID(string key, int st, int ed) {
-		int mid = (st + ed) / 2;
-		string temp = get(mid);
-		// cout << temp << endl; // debug
-		// cout << key << endl; // debug
-		// cout << temp.compare(0, 9, key) << endl; // debug
-		if (temp.compare(0, 9, key) == 0) {
-			removeAt(mid);
+class linkedList {
+public:
+	//head pointer
+	node* head;
+public:
+	linkedList() {
+		head = NULL;
+	}
+	bool isEmpty() {
+		return head == NULL ? true : false;
+	}
+	void printList() {
+		node *currNode = head;
+		while (currNode) {
+			cout << currNode->tconst << "\t" << currNode->titleType << "\t" << currNode->primaryTitle << "\t" << currNode->startYear << "\t" << currNode->runtimeMinutes << "\t";
+			for (int i = 0; i < currNode->genresCounter; i++) cout << currNode->genres[i] << "\t";
+			cout << endl;
+			currNode = currNode->next;
 		}
-		else if (st == mid || mid == ed) {
-			cout << "target not found" << endl;
+	}
+	void insertRecord(string tconst, string titleType, string primaryTitle, string startYear, string runtimeMinutes, string genres) {
+		node *newNode = new node(tconst, titleType, primaryTitle, startYear, runtimeMinutes, genres);
+		if (isEmpty()) {
+			head = newNode;
 		}
-		else if (temp.compare(0, 9, key) > 0) {
-			searchByID(key, st, mid - 1);
+		else {
+			node *currNode = head;
+			while (currNode->next) {
+				currNode = currNode->next;
+			}
+			currNode->next = newNode;
 		}
-		else searchByID(key, mid + 1, ed);
 	}
 
 };
 
 int main() {
-
-	DynamicArray unmei;
-	int position = 0;
-	string name;
+	linkedList unmei;
+	string fileName;
 	ifstream inputFile;
-	//can be developed as a control loop
 	while (true) {
-		cout << "enter the file path with xxx.txt format (e.g. DataSet1_1000.txt)" << endl;
-		cin >> name;
-		inputFile.open(name.c_str());
+		cout << "enter file path" << endl;
+		cin >> fileName;
+		inputFile.open(fileName.c_str());
 		if (inputFile.is_open()) {
 			string temp;
 			while (getline(inputFile, temp)) {
-				unmei.insertAt(position, temp);
-				//cout << unmei.get(position) << endl; //bebug message, can be deleted
-				position++;
+				int counter = 0;
+				string tconst, titleType, primaryTitle, startYear, runtimeMinutes, genres;
+				for (int i = 0; i < temp.size(); i++) {
+					if (temp[i] == 9) counter++;
+					else {
+						if (counter == 0) tconst += temp[i];
+						if (counter == 1) titleType += temp[i];
+						if (counter == 2) primaryTitle += temp[i];
+						if (counter == 3) startYear += temp[i];
+						if (counter == 4) runtimeMinutes += temp[i];
+						if (counter == 5) genres += temp[i];
+					}
+				}
+				unmei.insertRecord(tconst, titleType, primaryTitle, startYear, runtimeMinutes, genres);
 				temp.clear();
 			}
 			inputFile.close();
-			name.clear();
+			fileName.clear();
 		}
 		else {
-			cout << "Insert part ended. Move to delet part." << endl;
+			cout << "insert ended." << endl;
 			break;
 		}
+		unmei.printList();
 	}
-
-	//binary search
-	string theQuery;
-	while (true) {
-		cout << "support id search only, plz input as 'ttXXXXXXX' format " << endl;
-		cin >> theQuery;
-		if (theQuery.compare("end") == 0) {
-			cout << "delete part end. show updated record" << endl;
-			break;
-		}
-		unmei.searchByID(theQuery, 0, position - 1);
-		position--;
-		theQuery.clear();
-	}
-
-	//bebug message, can be deleted
-	for (int i = 0; i < position; i++) {
-		cout << unmei.get(i) << endl;
-	}
-
 	system("pause");
-
 	return 0;
 }
